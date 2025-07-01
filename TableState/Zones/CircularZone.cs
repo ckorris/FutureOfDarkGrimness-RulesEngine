f0b11348901cbc47559db3ece0706aacd7d0df71
Chris Orris <ckorris@gmail.com>
2025-06-30 21:53:44 -0500

    Added circular zone, tweaked map stage.

---
+++ TableState/Zones/CircularZone.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace FDG
+{
+    public class CircularZone : IZone
+    {
+        public readonly Float2 Center;
+
+        public readonly float Radius;
+
+        public CircularZone(Float2 center, float radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        public CircularZone(float xPosition, float yPosition, float radius)
+        {
+            Center = new Float2(xPosition, yPosition);
+            Radius = radius;
+        }
+
+        public bool DoesPathIntersectZone(Float2 startPosition, Float2 endPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPointWithinZone(Float2 position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

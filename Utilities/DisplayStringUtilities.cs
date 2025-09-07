5eb4225a9fa6f496d67895c21600ee82ed4d512f
Chris Orris <ckorris@gmail.com>
2025-09-07 17:30:32 -0500

    Made utilities for displaying stats on weapons and models easier.

---
+++ Utilities/DisplayStringUtilities.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDG.Utilities
+{
+    public static class DisplayStringUtilities
+    {
+        public static string BuildModelString(IModel model)
+        {
+            if (model.Weapons.Count == 0)
+            {
+                return " --- "; //Weird, but I guess possible.
+            }
+
+            StringBuilder sb = new StringBuilder(BuildWeaponString(model.Weapons[0]));
+
+            for (int i = 1; i < model.Weapons.Count; i++)
+            {
+                sb.Append(" | ");
+                sb.Append(BuildWeaponString(model.Weapons[i]));
+            }
+
+            return sb.ToString();
+        }
+
+        public static string BuildWeaponString(Weapon weapon)
+        {
+            StringBuilder sb = new StringBuilder($"{weapon.Name} - A{weapon.Attacks} AP{weapon.ArmorPenetration} ");
+
+            foreach (SpecialRule rule in weapon.SpecialRules)
+            {
+                sb.Append($"{rule.ToString()} ");
+            }
+
+            return sb.ToString();
+        }
+    }
+}

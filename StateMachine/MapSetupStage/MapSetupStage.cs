a527bad3dc2fd443818f7d3d6c29ca8419ec2114
ckorris <ckorris@gmail.com>
2026-05-06 17:49:39 -0500

    Add roll stages for terrain and objective placement order
    Three new stages inserted into MapSetupStage before their
    respective placement stages:
    - RollForFirstTerrainPlacementStage: roll-off to decide which
      team places terrain first
    - RollForObjectiveCountStage: rolls D3+2 to determine objective count
    - RollForFirstObjectivePlacementStage: roll-off to decide which
      team places objectives first
    
    Results are logged only for now; context storage and wiring into
    the place stages comes with their implementations.

--- StateMachine/MapSetupStage/MapSetupStage.cs
+++ StateMachine/MapSetupStage/MapSetupStage.cs
@@ -15,14 +15,20 @@ namespace FDG.Stages
             ToDeployment = new StageBinding(this);
 
             var dictionary = new TransitionSetBuilder(this)
+                .AddChild(new RollForFirstTerrainPlacementStage(GameContext, this), out var rollForFirstTerrain)
                 .AddChild(new PlaceTerrainStage(GameContext, this), out var placeTerrain)
+                .AddChild(new RollForObjectiveCountStage(GameContext, this), out var rollForObjectiveCount)
+                .AddChild(new RollForFirstObjectivePlacementStage(GameContext, this), out var rollForFirstObjective)
                 .AddChild(new PlaceObjectivesStage(GameContext, this), out var placeObjectives)
                 .AddSibling(nameof(ToDeployment), ToDeployment, out string toDeploymentEvent)
                 .Build();
 
-            startingChild = placeTerrain;
+            startingChild = rollForFirstTerrain;
 
-            placeTerrain.OnTerrainPlaced.Bind(placeObjectives);
+            rollForFirstTerrain.OnRollComplete.Bind(placeTerrain);
+            placeTerrain.OnTerrainPlaced.Bind(rollForObjectiveCount);
+            rollForObjectiveCount.OnRollComplete.Bind(rollForFirstObjective);
+            rollForFirstObjective.OnRollComplete.Bind(placeObjectives);
             placeObjectives.OnObjectivesPlaced.Bind(toDeploymentEvent);
 
             return dictionary;
